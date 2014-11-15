78c0554895009030c884cbf56efcb131e407a1b5
Sam Sudar <sudar.sam@gmail.com>
2014-11-15 14:34:46 -0500

    start adding UI

--- odk-sms-input/src/org/opendatakit/smsinput/activity/WelcomeActivity.java
+++ odk-sms-input/src/org/opendatakit/smsinput/activity/WelcomeActivity.java
@@ -5,7 +5,7 @@ import org.opendatakit.smsinput.R;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class WelcomeActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
